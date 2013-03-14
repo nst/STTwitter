d58cf0f7bb9808c7493e1f968fc77f440b1ed665
Nicolas Seriot <nicolas@seriot.ch>
2013-03-14 20:17:11 +0100

    user 'app only' authentication

--- ios/STTwitter_iOS/STTwitter_iOS/ViewController.m
+++ ios/STTwitter_iOS/STTwitter_iOS/ViewController.m
@@ -20,27 +20,45 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 
+//    // see http://rndc.or.id/wiki/index.php/(Ab)Using_Twitter_Client
+//    STTwitterAPIWrapper *twitter = [STTwitterAPIWrapper twitterAPIWithOAuthConsumerName:@"Tweetdeck"
+//                                                                            consumerKey:@"yT577ApRtZw51q4NPMPPOQ"
+//                                                                         consumerSecret:@"3neq3XqN5fO3obqwZoajavGFCUrC42ZfbrLXy5sCv8"
+//                                                                               username:@"username"
+//                                                                               password:@"password"];
+//    
+//    [twitter verifyCredentialsWithSuccessBlock:^(NSString *username) {
+//        
+//        NSLog(@"Access granted for %@", username);
+//
+//        [twitter getUserTimelineWithScreenName:@"barackobama" successBlock:^(NSArray *statuses) {
+//            NSLog(@"-- statuses: %@", statuses);
+//        } errorBlock:^(NSError *error) {
+//            NSLog(@"-- error: %@", error);
+//        }];
+//
+//    } errorBlock:^(NSError *error) {
+//        NSLog(@"-- error %@", error);
+//    }];
+
     // see http://rndc.or.id/wiki/index.php/(Ab)Using_Twitter_Client
-    STTwitterAPIWrapper *twitter = [STTwitterAPIWrapper twitterAPIWithOAuthConsumerName:@"Tweetdeck"
-                                                                            consumerKey:@"yT577ApRtZw51q4NPMPPOQ"
-                                                                         consumerSecret:@"xxxxxxxx"
-                                                                               username:@"username"
-                                                                               password:@"password"];
+    STTwitterAPIWrapper *twitter = [STTwitterAPIWrapper twitterAPIApplicationOnlyWithConsumerKey:@"yT577ApRtZw51q4NPMPPOQ"
+                                                                                  consumerSecret:@""];
     
     [twitter verifyCredentialsWithSuccessBlock:^(NSString *username) {
         
         NSLog(@"Access granted for %@", username);
-
+        
         [twitter getUserTimelineWithScreenName:@"barackobama" successBlock:^(NSArray *statuses) {
             NSLog(@"-- statuses: %@", statuses);
         } errorBlock:^(NSError *error) {
             NSLog(@"-- error: %@", error);
         }];
-
+        
     } errorBlock:^(NSError *error) {
         NSLog(@"-- error %@", error);
     }];
-    
+
 }
 
 - (void)didReceiveMemoryWarning
