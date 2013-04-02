992a2ec9c390bc022d727d3d3cbb8e0a380d8b53
Nicolas Seriot <nicolas.seriot@swissquote.com>
2013-04-02 14:19:34 +0200

    updated regex to find the authenticity_token value inside HTML form

--- STTwitter/STTwitterHTML.m
+++ STTwitter/STTwitterHTML.m
@@ -19,8 +19,8 @@
     r.completionBlock = ^(NSDictionary *headers, NSString *body) {
 
         NSError *error = nil;
-        NSString *token = [body firstMatchWithRegex:@"<input type=\"hidden\" name=\"authenticity_token\" value=\"(\\S+)\">" error:&error];
-        
+        NSString *token = [body firstMatchWithRegex:@"<input type=\"hidden\" value=\"(\\S+)\" name=\"authenticity_token\"/>" error:&error];        
+
         if(token == nil) {
             errorBlock(error);
             return;
