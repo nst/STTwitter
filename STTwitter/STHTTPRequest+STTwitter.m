5b3e9bb336a011b604d948ab4e7e268d6186bb52
Nicolas Seriot <nicolas.seriot@swissquote.ch>
2014-04-16 15:28:25 +0200

    updated STHTTPRequest

--- STTwitter/STHTTPRequest+STTwitter.m
+++ STTwitter/STHTTPRequest+STTwitter.m
@@ -33,7 +33,7 @@
     
     r.uploadProgressBlock = uploadProgressBlock;
     
-    r.downloadProgressBlock = ^(NSData *data, NSInteger totalBytesReceived, NSInteger totalBytesExpectedToReceive) {
+    r.downloadProgressBlock = ^(NSData *data, NSInteger totalBytesReceived, long long totalBytesExpectedToReceive) {
         
         if(downloadProgressBlock == nil) return;
         
