7a30f629629d5eb6475367a649ed08c6b9272d02
Nicolas Seriot <nicolas@seriot.ch>
2014-05-14 11:37:01 +0200

    improved travis build

---
+++ Tests/STTwitter/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  STTwitter
+//
+//  Created by Nicolas Seriot on 14/05/14.
+//  Copyright (c) 2014 Nicolas Seriot. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
