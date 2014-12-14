a0c3dda85144ff0a6a5f7d0ea806baea399d83de
Nicolas Seriot <nicolas@seriot.ch>
2014-12-14 14:47:50 +0100

    avoid a retain cycle by using a weak reference

--- STTwitter/STTwitterAppOnly.m
+++ STTwitter/STTwitterAppOnly.m
@@ -162,15 +162,18 @@
     
     //    NSString *requestID = [[NSUUID UUID] UUIDString];
     
+    __weak STHTTPRequest *wr = nil;
     __block STHTTPRequest *r = [STHTTPRequest twitterRequestWithURLString:urlString
                                              stTwitterUploadProgressBlock:nil
                                            stTwitterDownloadProgressBlock:^(id json) {
-                                               if(progressBlock) progressBlock(r, json);
+                                               if(progressBlock) progressBlock(wr, json);
                                            } stTwitterSuccessBlock:^(NSDictionary *requestHeaders, NSDictionary *responseHeaders, id json) {
                                                successBlock(r, requestHeaders, responseHeaders, json);
                                            } stTwitterErrorBlock:^(NSDictionary *requestHeaders, NSDictionary *responseHeaders, NSError *error) {
                                                errorBlock(r, requestHeaders, responseHeaders, error);
                                            }];
+    wr = r;
+    
     if(_bearerToken) {
         [r setHeaderWithName:@"Authorization" value:[NSString stringWithFormat:@"Bearer %@", _bearerToken]];
     }
