fb42b445842b3c61dd6ac3057e71673c1b219e93
Nicolas Seriot <nicolas@seriot.ch>
2015-03-01 16:58:27 +0100

    simplified the "followers" sample code by using the new fetchAndFollowCursorsForResource:.. method

--- demo_cli/followers/main.m
+++ demo_cli/followers/main.m
@@ -9,93 +9,45 @@
 #import <Foundation/Foundation.h>
 #import "STTwitter.h"
 
-typedef void (^AllFollowersBlock_t)(NSArray *allFollowers);
-
-void getFollowers(STTwitterAPI *twitter,
-                  NSString *screenName,
-                  NSMutableArray *followers,
-                  NSString *cursor,
-                  AllFollowersBlock_t allFollowersBlock) {
-    
-    if(followers == nil) followers = [NSMutableArray array]; 
-    
-    NSMutableDictionary *md = [NSMutableDictionary dictionary];
-    md[@"screen_name"] = screenName;
-    if(cursor) md[@"cursor"] = cursor;
-    md[@"skip_status"] = @"1";
-    md[@"include_user_entities"] = @"0";
-    
-    [twitter getResource:@"followers/list.json"
-           baseURLString:kBaseURLStringAPI_1_1
-              parameters:md
-   downloadProgressBlock:^(id json) {
-       //
-   } successBlock:^(NSDictionary *rateLimits, id response) {
-
-       NSArray *users = nil;
-       NSString *previousCursor = nil;
-       NSString *nextCursor = nil;
-       
-       if([response isKindOfClass:[NSDictionary class]]) {
-           users = [response valueForKey:@"users"];
-           previousCursor = [response valueForKey:@"previous_cursor_str"];
-           nextCursor = [response valueForKey:@"next_cursor_str"];
-       }
-       
-       NSLog(@"-- users: %@", @([users count]));
-       NSLog(@"-- previousCursor: %@", previousCursor);
-       NSLog(@"-- nextCursor: %@", nextCursor);
-       
-       [followers addObjectsFromArray:users];
-
-       if([nextCursor integerValue] == 0) {
-           allFollowersBlock(followers);
-           return;
-       }
-       
-       /**/
-       
-       NSString *remainingString = [rateLimits objectForKey:@"x-rate-limit-remaining"];
-       NSString *resetString = [rateLimits objectForKey:@"x-rate-limit-reset"];
-       
-       NSInteger remainingInteger = [remainingString integerValue];
-       NSInteger resetInteger = [resetString integerValue];
-       NSTimeInterval timeInterval = 0;
-       
-       if(remainingInteger == 0) {
-
-           NSDate *resetDate = [[NSDate alloc] initWithTimeIntervalSince1970:resetInteger];
-           timeInterval = [resetDate timeIntervalSinceDate:[NSDate date]] + 5;
-
-       }
-
-       NSLog(@"-- wait for %@ seconds", @(timeInterval));
-
-       dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(timeInterval * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-           
-           getFollowers(twitter, screenName, followers, nextCursor, allFollowersBlock);
-           
-       });
-       
-   } errorBlock:^(NSError *error) {
-       NSLog(@"-- error: %@", error);
-   }];
-}
-
 int main(int argc, const char * argv[])
 {
     
     @autoreleasepool {
         
-        STTwitterAPI *t = [STTwitterAPI twitterAPIOSWithFirstAccount];
+        __block NSMutableArray *followersIDs = [NSMutableArray array];
+        
+        STTwitterAPI *t = [STTwitterAPI twitterAPIWithOAuthConsumerKey:@""
+                                                        consumerSecret:@""
+                                                              username:@""
+                                                              password:@""];
         
         [t verifyCredentialsWithSuccessBlock:^(NSString *username) {
-        
-            AllFollowersBlock_t allFollowersBlock = ^(NSArray *allFollowers) {
-                NSLog(@"-- allFollowers: %@", [allFollowers valueForKey:@"screen_name"]);
-            };
             
-            getFollowers(t, @"SbClx", nil, nil, allFollowersBlock);
+            [t fetchAndFollowCursorsForResource:@"followers/ids.json"
+                                     HTTPMethod:@"GET"
+                                  baseURLString:@"https://api.twitter.com/1.1"
+                                     parameters:@{@"screen_name":@"0xcharlie"}
+                            uploadProgressBlock:nil
+                          downloadProgressBlock:nil
+                                   successBlock:^(id request, NSDictionary *requestHeaders, NSDictionary *responseHeaders, id response, BOOL morePagesToCome, BOOL *stop) {
+                                       NSLog(@"-- success, more to come: %d, %@", morePagesToCome, response);
+                                       
+                                       NSArray *ids = [response valueForKey:@"ids"];
+                                       if(ids) {
+                                           [followersIDs addObjectsFromArray:ids];
+                                           NSLog(@"-- added %lu IDs, more to come: %d", [ids count], morePagesToCome);
+                                       }
+                                       
+                                       if(morePagesToCome == NO) {
+                                           NSLog(@"-- IDs count: %lu", (unsigned long)[followersIDs count]);
+                                           NSLog(@"-- IDs: %@", followersIDs);
+                                       }
+                                       
+                                   } pauseBlock:^(NSDate *nextRequestDate) {
+                                       NSLog(@"-- rate limit exhausted, nextRequestDate: %@", nextRequestDate);
+                                   } errorBlock:^(id request, NSDictionary *requestHeaders, NSDictionary *responseHeaders, NSError *error) {
+                                       NSLog(@"-- %@", error);
+                                   }];
             
         } errorBlock:^(NSError *error) {
             NSLog(@"--1 %@", error);
