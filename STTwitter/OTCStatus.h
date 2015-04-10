2d1be42319cdff54260cdaba94b1b8242b106799
Tong Guo <Tong-G@outlook.com>
2015-04-10 14:05:15 +0800

    Implemented a batch properties

--- STTwitter/OTCStatus.h
+++ STTwitter/OTCStatus.h
@@ -15,8 +15,16 @@
 
     NSDate __strong* _dataCreated;
 
+    // Attributes
     BOOL _isFavoritedByMe;
     NSUInteger _favoriteCount;
+
+    BOOL _isRetweetedByMe;
+    NSUInteger _retweetCount;
+
+    // IDs
+    NSString __strong* _tweetIDString;
+    NSUInteger _tweetID;
     }
 
 @property ( retain, readonly ) NSDictionary* JSONArray;
@@ -30,6 +38,31 @@
 
 /** Indicates approximately how many times this Tweet has been “favorited” by Twitter users. */
 @property ( assign, readonly ) NSUInteger favoriteCount;
+
+/** Indicates whether this Tweet has been retweeted by the authenticating user.
+  */
+@property ( assign, readonly ) BOOL isRetweetedByMe;
+
+/** Number of times this Tweet has been retweeted. 
+
+  @discussion This field is no longer capped at 99 and will not turn into a String for “100+”
+  */
+@property ( assign, readonly ) NSUInteger retweetCount;;
+
+/** The string representation of the unique identifier for this Tweet. 
+
+  @discussion Your app should use this rather than the large integer returned by `tweetID`.
+  */
+@property ( copy, readonly ) NSString* tweetIDString;
+
+/** The unsigned integer representation of the unique identifier for this Tweet. 
+
+  @discussion This number is greater than 53 bits and some programming languages may have 
+              difficulty/silent defects in interpreting it.
+              Using a signed 64 bit integer for storing this identifier is safe.
+              Use `tweetIDString` for fetching the identifier to stay on the safe side.
+  */
+@property ( assign, readonly ) NSUInteger tweetID;
 
 #pragma mark Initialization
 + ( instancetype ) statusWithJSON: ( NSDictionary* )_JSONDict;
--- STTwitter/OTCStatus.m
+++ STTwitter/OTCStatus.m
@@ -16,8 +16,15 @@
 #pragma mark Attributes
 @synthesize dateCreated = _dataCreated;
 
+// Attributes
 @synthesize isFavoritedByMe = _isFavoritedByMe;
 @synthesize favoriteCount = _favoriteCount;
+@synthesize isRetweetedByMe = _isRetweetedByMe;
+@synthesize retweetCount = _retweetCount;
+
+// IDs
+@synthesize tweetIDString = _tweetIDString;
+@synthesize tweetID = _tweetID;
 
 #pragma mark Initialization
 + ( instancetype ) statusWithJSON: ( NSDictionary* )_JSONDict
@@ -35,6 +42,11 @@
 
         self->_isFavoritedByMe = [ self->_JSONDict[ @"favorited" ] boolValue ];
         self->_favoriteCount = [ self->_JSONDict[ @"favorite_count" ] unsignedIntegerValue ];
+        self->_isRetweetedByMe = [ self->_JSONDict[ @"retweeted" ] boolValue ];
+        self->_retweetCount = [ self->_JSONDict[ @"retweet_count" ] unsignedIntegerValue ];
+
+        self->_tweetIDString = [ self->_JSONDict[ @"id_str" ] copy ];
+        self->_tweetID = [ self->_JSONDict[ @"id" ] unsignedIntegerValue ];
         }
 
     return self;
