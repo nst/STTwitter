b9627540e966a951c8653eac94f31f2c32eb7ede
Tong Guo <Tong-G@outlook.com>
2015-04-15 00:41:15 +0800

    Backup

--- STTwitter/OTCTwitterUser.h
+++ STTwitter/OTCTwitterUser.h
@@ -24,7 +24,117 @@
 
 #import <Foundation/Foundation.h>
 
+@class OTCTweet;
+@class OTCEmbeddedURL;
+
+/** Twitter Users can be anyone or anything. They tweet, follow, create lists, have a home_timeline, 
+    can be mentioned, and can be looked up in bulk.
+  */
 @interface OTCTwitterUser : NSObject
+    {
+@private
+    NSUInteger _ID;
+    NSString* _IDString;
+
+    NSString* _displayName;
+    NSString* _screenName;
+
+    BOOL _isContributorsEnabled;
+    BOOL _isProtected;
+    BOOL _isVerified;
+
+    NSDate __strong* _dateCreated;
+    BOOL _usesDefaultTheme;
+    BOOL _usesDefaultAvatar;
+    BOOL _usesBackgroundImage;
+
+    NSString* _bio;
+    NSString* _location;
+    OTCEmbeddedURL __strong* _website;
+    OTCEmbeddedURL __strong* _URLEmbeddedInBio;
+
+    BOOL _profileBackgroundTile;
+    NSColor __strong* _profileBackgroundColor;
+    NSURL __strong* _profileBackgroundImageURL;
+    NSURL __strong* _profileBackgroundImageURLOverSSL;
+    NSURL __strong* _profileBannerURL;
+    NSURL __strong* _avatarImageURL;
+    NSURL __strong* _avatarImageURLOverSSL;
+
+    NSColor __strong* _profileLinkColor;
+    NSColor __strong* _profileSidebarBorderColor;
+    NSColor __strong* _profileSidebarFillColor;
+    NSColor __strong* _profileTextColor;
+
+    NSUInteger _favoritesCount;
+    NSUInteger _followersCount;
+    NSUInteger _followingsCount;
+    NSUInteger _listedCount;
+    NSUInteger _tweetsCount;
+
+    BOOL _sentFollowRequestByMe;
+    BOOL _isFollowing;
+
+    BOOL _isGeoEnabled;
+    NSString* _language;
+    NSTimeZone __strong* _timeZone;
+    NSInteger _UTCOffset;
+    NSString* _withheldInCountries;
+    NSString* _withheldScope;
+
+    OTCTweet __strong* _mostRecentTweet;
+    }
+
+@property ( assign, readonly ) NSUInteger ID;
+@property ( copy, readonly ) NSString* IDString;
+
+@property ( copy, readonly ) NSString* displayName;
+@property ( copy, readonly ) NSString* screenName;
+
+@property ( assign, readonly ) BOOL isContributorsEnabled;
+@property ( assign, readonly ) BOOL isProtected;
+@property ( assign, readonly ) BOOL isVerified;
+
+@property ( strong, readonly ) NSDate* dateCreated;
+@property ( assign, readonly ) BOOL usesDefaultTheme;
+@property ( assign, readonly ) BOOL usesDefaultAvatar;
+@property ( assign, readonly ) BOOL usesBackgroundImage;
+
+@property ( copy, readonly ) NSString* bio;
+@property ( copy, readonly ) NSString* location;
+@property ( strong, readonly ) OTCEmbeddedURL* website;
+@property ( strong, readonly ) OTCEmbeddedURL* URLEmbeddedInBio;
+
+@property ( assign, readonly ) BOOL profileBackgroundTile;
+@property ( strong, readonly ) NSColor* profileBackgroundColor;
+@property ( strong, readonly ) NSURL* profileBackgroundImageURL;
+@property ( strong, readonly ) NSURL* profileBackgroundImageURLOverSSL;
+@property ( strong, readonly ) NSURL* profileBannerURL;
+@property ( strong, readonly ) NSURL* avatarImageURL;
+@property ( strong, readonly ) NSURL* avatarImageURLOverSSL;
+
+@property ( strong, readonly ) NSColor* profileLinkColor;
+@property ( strong, readonly ) NSColor* profileSidebarBorderColor;
+@property ( strong, readonly ) NSColor* profileSidebarFillColor;
+@property ( strong, readonly ) NSColor* profileTextColor;
+
+@property ( assign, readonly ) NSUInteger favoritesCount;
+@property ( assign, readonly ) NSUInteger followersCount;
+@property ( assign, readonly ) NSUInteger followingsCount;
+@property ( assign, readonly ) NSUInteger listedCount;
+@property ( assign, readonly ) NSUInteger tweetsCount;
+
+@property ( assign, readonly ) BOOL sentFollowRequestByMe;
+@property ( assign, readonly ) BOOL isFollowing;
+
+@property ( assign, readonly ) BOOL isGeoEnabled;
+@property ( copy, readonly ) NSString* language;
+@property ( strong, readonly ) NSTimeZone* timeZone;
+@property ( assign, readonly ) NSInteger UTCOffset;
+@property ( copy, readonly ) NSString* withheldInCountries;
+@property ( copy, readonly ) NSString* withheldScope;
+
+@property ( strong, readonly ) OTCTweet* mostRecentTweet;
 
 @end
 
