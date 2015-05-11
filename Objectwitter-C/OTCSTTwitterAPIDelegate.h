4b82bfa777b0b8fa4861483c7dc70d82e9e1a81e
Tong Guo <Tong-G@outlook.com>
2015-05-12 01:15:49 +0800

    I'm going to watch movie 🎬

---
+++ Objectwitter-C/OTCSTTwitterAPIDelegate.h
@@ -0,0 +1,18 @@
+//
+//  OTCSTTwitterAPIDelegate.h
+//  TwitterAPILab
+//
+//  Created by Tong G. on 5/12/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@class STTwitterAPI;
+
+@protocol OTCSTTwitterAPIDelegate
+
+//- ( void ) twitterAPI: ( STTwitterAPI* )_TwitterAPI
+
+
+@end
